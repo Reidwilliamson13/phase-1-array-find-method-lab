38f5a56f93d4e78fe6b884321c4fa27b95c6ba9f
ReidWilliamson <reidwilliamson2@gmail.com>
2021-09-27 16:59:04 -0400

    commit tree 67b540d8eb8f87018bde9948486fdc75d415565a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// function superbowlWin(array) {
+//   let rings = array.find((array) => array.result === "W");
+//   if (rings) {
+//     return rings.year;
+//   }
+// }
+// console.log(superbowlWin(record));
 
+function superbowlWin(array) {
+  let rings = array.find((array) => array.result === "W");
+  if (rings) {
+    return rings.year;
+  }
+}
+console.log(superbowlWin(record));
